ab20088045afbc32469614fe71e8916359a0f2b7
Ahmed Shalash <ah.shalash@gmail.com>
2024-02-06 03:08:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  word = word.toLowerCase();
+  let start = 0;
+  let end = word.length - 1;
+
+
+  while (start < end) {
+    if (word[start] !== word[end]) {
+      return false;
+    }
+    start++;
+    end--;
+  }
+  return true;
 }
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
+// Test cases
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
